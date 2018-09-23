9052e3b6d1fb59ea247098481d708a271613ba48
Gustavo Galarreta <gustavogalarreta@Gustavos-MacBook-Pro.local>
2018-09-23 17:58:53 -0500

    api service refactor

--- app/models/services/api_service.rb
+++ app/models/services/api_service.rb
@@ -14,13 +14,7 @@ class ApiService
 
   def post body, params
     begin
-      url_service = end_point_handler params
-      headers_service = token_handler params
-      service_response = RestClient::Request.execute(method: :post, url: url_service, payload: body, timeout: timeout, open_timeout: open_timeout, headers: headers_service)
-      response = {
-        "status": service_response.code,
-        "data": JSON.parse(service_response)
-      }
+      request_handler({method: :post, payload: body}, params)
     rescue Exception => e
       p e
     end
@@ -28,13 +22,7 @@ class ApiService
 
   def get params
     begin
-      url_service = end_point_handler params
-      headers_service = token_handler params
-      service_response = RestClient::Request.execute(method: :get, url: url_service, timeout: timeout, open_timeout: open_timeout, headers: headers_service)
-      response = {
-        "status": service_response.code,
-        "data": JSON.parse(service_response)
-      }    
+      request_handler({method: :get}, params)
     rescue Exception => e
       p e
     end
@@ -42,29 +30,32 @@ class ApiService
 
   def delete params
     begin
-      url_service = end_point_handler params
-      headers_service = token_handler params
-      service_response = RestClient::Request.execute(method: :delete, url: url_service, timeout: timeout, open_timeout: open_timeout, headers: headers_service)
-      response = {
-        "status": service_response.code,
-        "data": JSON.parse(service_response)
-      }
+      request_handler({method: :delete}, params)
     rescue Exception => e
       p e
     end
   end
 
   def put body, params
-    url_service = end_point_handler params
-    headers_service = token_handler params 
-    service_response = RestClient::Request.execute(method: :put, url: url_service, payload: body, timeout: timeout, open_timeout: open_timeout, headers: headers_service)
-    response = {
-      "status": service_response.code,
-      "data": JSON.parse(service_response)
-    }      
+    request_handler({method: :put, payload: body}, params)   
   end
 
   private
+
+    def request_handler request, params
+      request_params = Hash.new
+      request_params[:method] = request[:method]
+      request_params[:url] = end_point_handler params
+      request_params[:timeout] = timeout
+      request_params[:open_timeout] = open_timeout
+      request_params[:headers] = token_handler params
+      request_params[:payload] = request[:payload] if request[:payload]
+      service_response = RestClient::Request.execute(request_params)
+      response = {
+        "status": service_response.code,
+        "data": JSON.parse(service_response)
+      }
+    end
 
     def token_handler params
       headers_service = headers
