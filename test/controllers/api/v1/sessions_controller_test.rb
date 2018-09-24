746e04ad7c21ec03dd7a5d03f3296f872b995643
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2018-09-24 08:51:32 -0500

    user test

--- test/controllers/api/v1/sessions_controller_test.rb
+++ test/controllers/api/v1/sessions_controller_test.rb
@@ -4,4 +4,12 @@ class Api::V1::SessionsControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  #test "create should be success" do
+    #session = sessions(:session)
+
+    #get :create, 
+
+    #assert_response :success
+  #end
 end
--- test/controllers/api/v1/users_controller_test.rb
+++ test/controllers/api/v1/users_controller_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class Api::V1::UsersControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create action should be success" do
+    user = users(:user)
+
+    post :create, user: user
+
+    assert_reponse :success
+  end
+ #test "index action should be success" do
+    #get :index
+    #assert_reponse :success    
+ #end
+
+ #test "show action should be success" do
+ #   user = users(:user)
+ #   get :show, id: user.id
+ #   assert_reponse :success
+ #end
+
 end
