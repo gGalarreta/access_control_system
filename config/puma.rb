6198c7ad5e2050bc9de2f997cf05e79d02b6acd2
Gustavo Galarreta <gustavo.galarreta@pucp.pe>
2018-09-28 10:44:40 -0500

    puma

--- config/puma.rb
+++ config/puma.rb
@@ -9,21 +9,11 @@ port        ENV['PORT']     || 3000
 environment ENV['RACK_ENV'] || 'development'
 
 on_worker_boot do
-  # worker specific setup
-  #ActiveSupport.on_load(:active_record) do
-  #  config = ActiveRecord::Base.configurations[Rails.env] ||
-  #              Rails.application.config.database_configuration[Rails.env]
-  #  config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
-  #  ActiveRecord::Base.establish_connection(config)
-  #end
-  ActiveRecord::Base.connection_pool.disconnect!
-  
   ActiveSupport.on_load(:active_record) do
-    if Rails.application.config.database_configuration
-      config = Rails.application.config.database_configuration[Rails.env]
-      config['reaping_frequency'] = ENV['DB_REAP_FREQ'] || 10 # seconds
-      config['pool']              = ENV['DB_POOL']      || 5
-      ActiveRecord::Base.establish_connection(config)
-    end
+    config = ActiveRecord::Base.configurations[Rails.env] ||
+                Rails.application.config.database_configuration[Rails.env]
+    config['pool'] = ENV['RAILS_MAX_THREADS'] || 5
+    ActiveRecord::Base.establish_connection(config)
   end
+
 end
